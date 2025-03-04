c978d39e8d50821382000ae12253d5f6df8e00d9
jhayashi1 <jaredhayashi@gmail.com>
2025-03-04 17:31:03 -0600

    fix: add friend request to route controller

--- src/friend/index.ts
+++ src/friend/index.ts
@@ -1,9 +1,10 @@
 import type {Router} from '../shared/endpoint-types';
 import {apiGatewayHandler} from '../shared/api-gateway-handler';
+import {FriendRequestRouteController} from './endpoints/friend-request-route-controller';
 
 const router: Router = {
     // 'GET /friend/list'     : GetSessionRouteController,
-    // 'POST /friend/request' : StartSessionRouteController,
+    'POST /friend/request': FriendRequestRouteController,
     // 'POST /friend/accept'  : StopSessionRouteController,
     // 'POST /friend/decline' : StopSessionRouteController,
     // 'POST /friend/remove'  : StopSessionRouteController,
